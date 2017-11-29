f635d723d27a9e43bc18ac9eb8bb2c42e9dc8170
George <gabolaev98@gmail.com>
2017-11-30 02:55:40 +0300

    2_6_1

---
+++ tpark-algo/2 module/6_1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+#define ALPHABET_SIZE 256
+
+void countingSort(std::vector<std::string> &words, int from, int to, int letterIndex,
+                  std::array<int, ALPHABET_SIZE + 1> &counters) {
+
+    for (auto i = from; i < to; ++i)
+        ++counters[words[i][letterIndex]];
+
+    int forLast = 0, tmp;
+    for (auto i = 0; i < ALPHABET_SIZE; ++i) {
+        tmp = counters[i];
+        counters[i] = forLast;
+        forLast += tmp;
+    }
+    counters[ALPHABET_SIZE] = forLast;
+
+    std::vector<std::string> temp(static_cast<unsigned long>(to - from));
+
+    for (auto i = from; i < to; ++i)
+        temp[counters[words[i][letterIndex]]++] = words[i];
+
+    for (auto i = from; i < to; ++i)
+        words[i] = temp[i - from];
+
+}
+
+void MSDSort(std::vector<std::string> &words, int from, int howMuch, int letterIndex) {
+    if (howMuch <= 1) return;
+
+    std::array<int, ALPHABET_SIZE + 1> counters{};
+
+    countingSort(words, from, from + howMuch, letterIndex, counters);
+
+    for (int i = 0; i < ALPHABET_SIZE; ++i)
+        MSDSort(words, from + counters[i], counters[i + 1] - counters[i], letterIndex + 1);
+}
+
+int main() {
+
+    std::vector<std::string> words;
+    std::string input;
+    while (std::cin >> input)
+        words.push_back(input);
+
+    MSDSort(words, 0, static_cast<int>(words.size()), 0);
+
+    for (auto &word : words)
+        std::cout << word << std::endl;
+
+    return 0;
+}
